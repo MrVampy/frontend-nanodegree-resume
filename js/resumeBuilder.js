6b2cd819f4ed51e4caa05529ce2b34e365af16fa
MrVampy <stenhoejparken90@gmail.com>
2014-11-12 21:59:55 +0100

    Up to date commit

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,22 +1,50 @@
 var bio = {
     "name":"Thomas",
     "role":"Software Developer",
-    "contact":"+4542720996",
+    "contacts": {
+	"mobile":"+4542720996",
+	"email":"thomas@baaschandersen.com",
+	"github":"https://github.com/MrVampy",
+	"location":"Borup, Denmark"
+    },
     "picture":"images/thomas.jpg",
     "welcome":"Hello and welcome to my resume.",
-    "skills":"You name it and I can do it!"
+    "skills": [
+	"You name it and I can do it!","Loving the work","Making things automated"
+    ]
 }
 
-var formattedName = HTMLheaderName.replace("%data%",bio.name);
-var formattedRole = HTMLheaderRole.replace("%data%",bio.role);
-var formattedContact = HTMLcontactGeneric.replace("%data%",bio.contact);
-var formattedPicture = HTMLbioPic.replace("%data%",bio.picture);
-var formattedWelcome = HTMLWelcomeMsg.replace("%data%",bio.welcome);
-var formattedSkills = HTMLskills.replace("%data%",bio.skills);
+var work = {
+    "dates":"February 2014",
+    "description":"Doing various software developer tasks with an ERP system called Microsoft Dynamics NAV",
+    "employer":"Logos Consult A/S",
+    "location":"Alleroed, Nordsjaelland, Denmark",
+    "title":"Software developer"
+}
 
-$("#header").append(formattedName);
-$("#header").append(formattedRole);
-$("#header").append(formattedContact);
-$("#header").append(formattedPicture);
-$("#header").append(formattedWelcome);
-$("#header").append(formattedSkills);
+
+var education = {
+    "name":"Copenhagen Business School",
+    "dates":"September 2007 - March 2013",
+    "location":"Frederiksberg, Copenhagen",
+    "major":"Business Administration and IT"
+}
+
+var project = {
+    "name":"Resume",
+    "type":"Website"
+}
+
+
+if(bio.skills.length > 0) {
+    $("#header").append(HTMLskillsStart);
+
+    var formattedSkill = HTMLskills.replace("%data",bio.skills[0]);
+    $("#skills").append(formattedSkill);
+    formattedSkill = HTMLskills.replace("%data",bio.skills[1]);
+    $("#skills").append(formattedSkill);
+    formattedSkill = HTMLskills.replace("%data",bio.skills[2]);
+    $("#skills").append(formattedSkill);
+    formattedSkill = HTMLskills.replace("%data",bio.skills[3]);
+    $("#skills").append(formattedSkill);
+}
